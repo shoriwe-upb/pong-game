588347b35d075e4f4550c02aa609f3af3e6b38cf
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2019-10-08 15:06:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import objects
 
 
 def main():
-    screen = objects.Window()
-    screen.run()
-
+    try:
+        while True:
+            objects.Window().run()
+            if "y" not in input("Do you want to play again?[y/n]"):
+                break
+    except:
+        pass
 
 if __name__ == '__main__':
     main()
